493d2c3978edfc60df7ff5e65e77e91e1bcc8cc1
ijTuhin <isratishu0209@gmail.com>
2022-04-12 02:03:01 +0600

    successful login using DB

---
+++ backendApi/sign-in.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    include("connectDB.php");
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $check = mysqli_query($connect, "SELECT * FROM signup WHERE email='$email' AND password='$password'");
+    if(mysqli_num_rows($check) > 0){
+      echo '
+          <script>
+          alert("Welcome to wander");
+            window.location = "../backend/dashboard.html";
+          </script>
+        ';
+    }
+    else{
+        echo '
+          <script>
+            alert("USER NOT FOUND!!");
+            window.location = "../";
+          </script>
+        ';
+    }
+?>
