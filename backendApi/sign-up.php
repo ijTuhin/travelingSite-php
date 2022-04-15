3704cde42d536fcc1523a482dc23e57360a32a2e
ijTuhin <isratishu0209@gmail.com>
2022-04-15 15:39:14 +0600

    unique email id for each new user

--- backendApi/sign-up.php
+++ backendApi/sign-up.php
@@ -9,25 +9,39 @@ $image = $_FILES['image']['name'];
 $tmp_name = $_FILES['image']['tmp_name'];
 
 if ($password == $cpassword) {
-    move_uploaded_file($tmp_name, "../user-img/$image");
-    $insert = mysqli_query($connect, "INSERT INTO user(user_name, user_phone, user_email, password, image) VALUES('$user_name','$user_phone', '$user_email', '$password','$image')");
 
-    if ($insert) {
+    $check = mysqli_query($connect, "SELECT * FROM user WHERE user_email='$user_email'");
+    if (mysqli_num_rows($check) > 0) {
         echo '
+            <script>
+                alert("User already exists! Please Login to continue");
+                window.location = "../index.html";
+            </script>
+        ';
+    }
+
+    else {
+        move_uploaded_file($tmp_name, "../user-img/$image");
+        $insert = mysqli_query($connect, "INSERT INTO user(user_name, user_phone, user_email, password, image) VALUES('$user_name','$user_phone', '$user_email', '$password','$image')");
+
+        if ($insert) {
+            echo '
                 <script>
                     alert("Registration Successful!");
-                    window.location = "../";
+                    window.location = "../backend/main-home.html";
                 </script>
             ';
-    }
-    else {
-        echo '
+        }
+        else {
+            echo '
                 <script>
                     alert("Error!!");
                     window.location = "../backend/sign-up.html";
                 </script>
             ';
+        }
     }
+
 }
 else {
     echo '
